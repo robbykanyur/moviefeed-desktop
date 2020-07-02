e46b36b6146f9e1835b987831aabd8c2c74c664b
Robert Kanyur <robert@kanyur.com>
2020-07-02 04:39:27 -0700

    Fetch from rails backend

--- src/App.js
+++ src/App.js
@@ -1,58 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/reviews')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            reviews: result
+          });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+  render() {
+    const { error, isLoaded, reviews } = this.state;
+    return (
+      <>
+        <h1>moviefeed</h1>
+        <hr />
+        <h2>latest reviews</h2>
+        {reviews.map(review => (
+          <div key={review.id}>
+            <strong>{review.movie.title}</strong>
+            <br />
+            <em>{review.user.username}</em>
+            <br />
+            Rating: {review.rating}
+            <br />
+            &ldquo;{review.review}&rdquo;
+            <br /><br />
+          </div>
+        ))}
+      </>
+    )
+  }
 }
 
 export default App;
